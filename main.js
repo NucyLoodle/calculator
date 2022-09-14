8636d1d3a5848c5c75d9aecae80ec5108b158b78
Loodle <lucymeredith3@gmail.com>
2022-09-14 05:44:47 +0100

    Write function to subtract numbers

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ const b = 11
 
 sum = (a,b) => a + b; //Add two numbers together
 console.log(sum(a,b)); //For testing
+
+subtract = (a,b) => a - b; //Subtract one number from another
+console.log(subtract(a,b)); //For testing
